c454a0d56dcfff1eea928c12fbabe21a28de600f
Mattia Polli <77399568+MattiaPolli93@users.noreply.github.com>
2021-10-08 19:40:08 +0200

    Add formatted dates, store functions into a separate file and refactor

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const account1 = {
         "2020-05-08T14:11:59.604Z",
         "2020-05-27T17:01:17.194Z",
         "2021-07-11T23:36:17.929Z",
-        "2021-07-12T10:51:36.790Z",
+        "2021-10-07T10:51:36.790Z",
     ],
     currency: "EUR",
     locale: "it-IT",
@@ -33,7 +33,7 @@ const account2 = {
         "2020-02-05T16:33:06.386Z",
         "2021-04-10T14:43:26.374Z",
         "2021-06-25T18:49:59.371Z",
-        "2021-07-26T12:01:20.894Z",
+        "2021-10-05T12:01:20.894Z",
     ],
     currency: "USD",
     locale: "en-US",
@@ -81,85 +81,7 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-// Banking movements
-const displayMovements = function(acc, sort = false) {
-    containerMovements.innerHTML = "";
-
-    const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
-
-    movs.forEach(function(mov, i) {
-        const type = mov > 0 ? "deposit" : "withdrawal";
-
-        const date = new Date(acc.movementsDates[i]);
-        const day = `${date.getDate()}`.padStart(2, "0");
-        const month = `${date.getMonth() + 1}`.padStart(2, "0");
-        const year = date.getFullYear();
-        const displayDate = `${day}/${month}/${year}`;
-
-        const html = `
-            <div class="movements__row">
-                <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-                <div class="movements__date">${displayDate}</div>
-                <div class="movements__value">${mov.toFixed(2)}€</div>
-            </div>
-        `;
-
-        containerMovements.insertAdjacentHTML("afterbegin", html);
-    });
-};
-
-// Display calculated balance
-const calcDisplayBalance = function(acc) {
-    acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
-};
-
-// Show summaries below
-const calcDisplaySummary = function(acc) {
-    const incomes = acc.movements
-        .filter(mov => mov > 0)
-        .reduce((acc, mov) => acc + mov, 0);
-    labelSumIn.textContent = `${incomes.toFixed(2)}€`;
-
-    const out = acc.movements
-        .filter(mov => mov < 0)
-        .reduce((acc, mov) => acc + mov, 0);
-    labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
-
-    const interest = acc.movements
-        .filter(mov => mov > 0)
-        .map(deposit => (deposit * acc.interestRate) / 100)
-        .filter(int => {
-            return int >= 1;
-        })
-        .reduce((acc, int) => acc + int, 0);
-    labelSumInterest.textContent = `${interest.toFixed(2)}€`;
-};
-
-// Create Username
-const createUsernames = function(accs) { // Accounts
-    accs.forEach(function(acc) {
-        acc.username = acc.owner
-        .toLowerCase()
-        .split(" ")
-        .map(name => name[0])
-        .join("");
-    })
-};
-
 createUsernames(accounts);
-
-// Update UI (movements, balance, summary)
-const updateUI = function(acc) {
-    // Display movements
-    displayMovements(acc);
-
-    // Display balance
-    calcDisplayBalance(acc);
-
-    // Display summary
-    calcDisplaySummary(acc);
-};
 
 // Event handlers
 let currentAccount;
@@ -177,12 +99,25 @@ btnLogin.addEventListener("click", function(e) {
 
         // Create current date and time
         const now = new Date();
+        const options = {
+            hour: "numeric",
+            minute: "numeric",
+            day: "numeric",
+            month: "numeric",
+            year: "numeric",
+            // weekday: "long"
+        };
+        // const locale = navigator.language;
+
+        labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now);
+
+        /* const now = new Date();
         const day = `${now.getDate()}`.padStart(2, "0");
         const month = `${now.getMonth() + 1}`.padStart(2, "0");
         const year = now.getFullYear();
         const hour = `${now.getHours()}`.padStart(2, "0");
         const min = `${now.getMinutes()}`.padStart(2, "0");
-        labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+        labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`; */
 
         // Clear input fields
         inputLoginUsername.value = inputLoginPin.value = "";
